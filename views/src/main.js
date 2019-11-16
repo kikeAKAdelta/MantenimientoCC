3ba0460874ba9c304b6945f959b7ad20fb6640b5
Ricardo Barrientos <ricky@ricky-debian.debian-r.com>
2019-11-16 10:01:13 -0600

    Solucionar error vue is not defined

--- views/src/main.js
+++ views/src/main.js
@@ -7,6 +7,7 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+window.Vue = Vue;
 
 Vue.config.productionTip = false;
 
--- views/src/views/OrdenCompra.vue
+++ views/src/views/OrdenCompra.vue
@@ -1,21 +1,25 @@
 <template>
-  <div class="container" id="tabla_datos">
+  <div class="container" id="ordencompra">
     <h2 class="container">Orden de Compra</h2>
     <table class="table">
       <thead>
         <tr>
+          <th scope="col">N° Compra</th>
+          <th scope="col">Código Material</th>
           <th scope="col">Material</th>
-          <th scope="col">Cantidad</th>
+          <th scope="col">Cantidad a comprar</th>
           <th scope="col">Estado</th>
           <th scope="col">Fecha</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <th scope="row">Destornillador</th>
-          <td>30</td>
-          <td>Completado</td>
-          <td>20/11/2019</td>
+        <tr v-for="item in lists" v-bind:key="item">
+          <th scope="row" v-text="item.id"></th>
+          <td v-text="item.name">30</td>
+          <td v-text="item.username">30</td>
+          <td v-text="item.email">30</td>
+          <td v-text="item.phone">30</td>
+          <td v-text="item.website">30</td>
         </tr>
         <tr>
           <th scope="row">Destornillador 2</th>
@@ -42,3 +46,23 @@
     </button>
   </div>
 </template>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.10/vue.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue-resource/1.5.1/vue-resource.min.js"></script>
+<script type="text/javascript">
+    var urlOrden= 'https://jsonplaceholder.typicode.com/todos/1';
+    new Vue({
+    el: '#ordencompra',
+    data: {
+      lists: []
+    },
+    methods: {
+      getOrden: function() {
+        this.$http.get(urlOrden).then(function(response){
+          this.lists = response;
+        });
+      }
+    }
+});
+
+</script>

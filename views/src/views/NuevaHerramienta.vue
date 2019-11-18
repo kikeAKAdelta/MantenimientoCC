81042b5bd55b129db7e552a81f8244a2183783a0
Ricardo Barrientos <ricky@ricky-debian.debian-r.com>
2019-11-18 08:48:52 -0600

    POST Herramienta que no sirve

--- views/src/views/NuevaHerramienta.vue
+++ views/src/views/NuevaHerramienta.vue
@@ -7,12 +7,12 @@
               </span>
             <h4 class="card-tittle">AGREGAR NUEVO</h4>
             <div class="employee-form">
-                <input id="nombre" type="text" class="form-control mb-2" placeholder="HERRAMIENTA">
-                <input id="cantidad" type="text" class="form-control mb-2" placeholder="CANTIDAD">
+                <input id="nombre" type="text" class="form-control mb-2" placeholder="HERRAMIENTA" v-model="Herramienta.nombre">
+                <input id="cantidad" type="text" class="form-control mb-2" placeholder="CANTIDAD" v-model="Herramienta.cantidad">
             </div>
           </div>
           <div class = "botonera">
-            <button type="button" class="btn btn-success" style='width:150px; height:50px' id="agregar">
+            <button type="button" class="btn btn-success" style='width:150px; height:50px' id="agregar" @click="getPosts()">
                 Agregar
             </button>
         </div>
@@ -21,14 +21,46 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
+  name: "Herramientas",
+  data() {
+    return{
+      Herramienta: {nombre:'', cantidad:''},
+    };
+  },
   methods: {
     limpiar(){
         document.getElementById("nombre").value = "";
         document.getElementById("cantidad").value = "";
-    }
-  }
-};
+    },
+    getPosts() {
+          let nuevaHerramienta = JSON.stringify({
+            nombre: this.Herramienta.nombre,
+            cantidad: this.Herramienta.cantidad
+          });
+          console.log(nuevaHerramienta);
+          axios.post('http://localhost:8181/MantenimientoAcc-Back/webresources/herramientas/', nuevaHerramienta
+          ,{
+            headers: {
+              "Access-Control-Allow-Origin":"*",
+              "Accept": "application/json",
+             'Content-Type': 'application/json',
+              "Authorization": "Bearer token",
+              },
+              method:"POST"
+          })
+          .then((response) => {
+            console.log(response);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+        },
+      }, mounted() {
+          this.getPosts();
+      }
+    };
 </script>
 
 
